0680acfba98326b9302ff992c02c12b97c5706c6
Webrowse <great.adarsh@gmail.com>
2026-02-18 22:32:15 +0530

    Phase 6: Improve report to show the changed field

--- src/report/writer.rs
+++ src/report/writer.rs
@@ -3,14 +3,57 @@ use crate::analysis::engine::AnalysisResult;
 pub fn print_text(result: &AnalysisResult) {
     println!("–––––––Retry Safety Report–––––––");
 
+    println!("Account: {}", result.before.pubkey);
+
     println!("Safety: {:?}", result.classification.safety);
 
     if result.classification.reasons.is_empty() {
-        println!("No persistent state changes detected..");
-    } else {
-        println!("Reasons: ");
-        for r in &result.classification.reasons {
-            println!("- {}", r);
-        }
+        println!("No state changes detected");
+        return;
     }
+    
+    println!("\nState Changes:");
+
+    // Lamport changed
+    if result.before.lamports != result.after.lamports {
+        println!(
+            "- Lamports: {} -> {}",
+            result.before.lamports, result.after.lamports
+        );
+
+    }
+
+    // Owner 
+    if result.before.owner != result.after.owner {
+        println!(
+            "Owner: {} -> {}",
+            result.before.owner, result.after.owner
+        );
+
+    }
+
+    // Executable
+        if result.before.executable != result.after.executable {
+        println!(
+            "- Executable: {} -> {}",
+            result.before.executable,
+            result.after.executable
+        );
+    }
+
+    // Data Size
+    if result.before.data_len != result.after.data_len {
+        println!(
+            "Data Size: {} -> {}",
+            result.before.data_len, result.after.data_len
+        );
+
+    }
+
+    
+    println!("\nReasons: ");
+    for r in &result.classification.reasons {
+        println!("- {}", r);
+    }
+    
 }
